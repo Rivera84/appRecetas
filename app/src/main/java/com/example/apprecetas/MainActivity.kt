8b3905fba221a5246640fa60fbbea0bae1192d96
Luisr <luisrivera084@gmail.com>
2019-11-25 21:48:57 -0600

    Nuevo diseño de pantallas inicioUsuario

--- app/src/main/java/com/example/apprecetas/MainActivity.kt
+++ app/src/main/java/com/example/apprecetas/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.apprecetas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnInicioUsuario.setOnClickListener(){
+            val intent = Intent(this,inicioSesioUsuario ::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
