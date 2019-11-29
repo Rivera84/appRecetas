211c7649480e767653eb98d807cc2de93844d4a0
Luisr <luisrivera084@gmail.com>
2019-11-28 22:30:16 -0600

    Se creo la pantalla de nuevoUsuario

--- app/src/main/java/com/example/apprecetas/inicioSesioUsuario.kt
+++ app/src/main/java/com/example/apprecetas/inicioSesioUsuario.kt
@@ -1,12 +1,20 @@
 package com.example.apprecetas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_inicio_sesio_usuario.*
 
 class inicioSesioUsuario : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesio_usuario)
+
+        btnCrearCuenta.setOnClickListener(){
+            val intent= Intent(this, nuevoUsuario ::class.java )
+            startActivity(intent)
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/apprecetas/nuevoUsuario.kt
@@ -0,0 +1,12 @@
+package com.example.apprecetas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nuevoUsuario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_usuario)
+    }
+}
